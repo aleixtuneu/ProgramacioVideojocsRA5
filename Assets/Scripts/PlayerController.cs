d51c0b35ee01b5db42c72dccc4fef01fc6c762ee
Aleix <aleix.tuneu.7e7@itb.cat>
2025-12-12 16:43:14 +0100

    added walking feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MoveBehaviour))]
+
+public class PlayerController : MonoBehaviour, InputSystem_Actions.IPlayerActions
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] protected MoveBehaviour _mb;
+    [SerializeField] protected Animator _animator;
+
+    private InputSystem_Actions _inputActions;
+
+    public void Awake()
     {
-        
+        _mb = GetComponent<MoveBehaviour>();
+        _animator = GetComponent<Animator>();
+        _inputActions = new InputSystem_Actions();
+        _inputActions.Player.SetCallbacks(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _inputActions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 moveInput = context.ReadValue<Vector2>();
+        _mb.SetInputDirection(moveInput);
+
+        bool isMoving = moveInput.sqrMagnitude > 0.1f;
+        if (_animator != null)
+        {
+            _animator.SetBool("IsWalking", isMoving);
+        }
     }
 }
